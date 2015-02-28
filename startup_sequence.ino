ed8bf52d5ce1259419a330d8a13ef916ddb73dbf
jlichty <jacob.lichty@tophatmonocle.com>
2015-02-28 14:31:17 -0500

    Integrated P-Control + BufferedSerial

--- startup_sequence.ino
+++ startup_sequence.ino
@@ -1,19 +1,40 @@
-#include <Servo.h> 
+#include <BufferedSerial.h>
+#include <ByteBuffer.h>
 #include <TimerOne.h>
+#include <Servo.h> 
 
-//
-volatile uint16_t timeFromLastPulse =0;//holds value since last pulse
+#define clock_speed 16000000
+BufferedSerial serial = BufferedSerial(256, 256);
+ByteBuffer send_buffer;
+
+//Use Arduino MEGA
+volatile uint16_t timeFromLastPulse = 0;//holds value since last pulse
 volatile uint8_t stopped_flag = 0; 
-volatile uint8_t timer_interrupt_flag = 0;
-volatile uint16_t lowPulseTime=0;
+volatile uint16_t lowPulseTime = 0;
+
+//***PID***
+volatile uint32_t tach_speed = 0;
+uint32_t setpoint_speed = 7400;
+const float Kp = 1;
+const float LSF = 0.002;
+const uint32_t UPPER_RPM_TH = 8000;
+const uint32_t LOWER_RPM_TH = 500;
+
+const uint32_t timer1_duration = 200000; // 20ms
+volatile uint8_t serial_write_counter = 0;
+volatile uint8_t controller_update_flag = 0;
+const uint8_t serial_write_threshold = 1000000/timer1_duration - 1;
 
 //Create object for servo library
 Servo engine_throttle_servo;
+volatile uint8_t servo_angle=0;
+const uint8_t UPPER_SERVO_TH = 150;
+const uint8_t LOWER_SERVO_TH = 130;
+
 Servo choke_servo;
 Servo esc_throttle_servo;
 
-void initialize_servo()//function to initialize servo motors
-{
+void initialize_servo() { //function to initialize servo motors
   cli();
   // INIT SERVOS
   engine_throttle_servo.attach(9);
@@ -21,13 +42,13 @@ void initialize_servo()//function to initialize servo motors
   esc_throttle_servo.attach(11);
   sei();
 }
-void initialize_timers()
-{
+
+void initialize_timers() {
   cli();//stop interrupts
 
   // INIT TIMER1 INTERRUPT
-  Timer1.initialize(500000);
-  Timer1.attachInterrupt(sendSerial_Timer_ISR);
+  Timer1.initialize(timer1_duration);
+  Timer1.attachInterrupt(setFlags_Timer_ISR);
 
   // INIT TIMER5
   //this timer is set to input capture mode. When it receives a rising edge on pin ICP5 
@@ -51,60 +72,61 @@ void initialize_timers()
 #define Gen_Esc_Rotor A2
 #define Esc_Power A3
 #define Ecu_Power A0
+
 void setup() {
-
-initialize_servo();
-initialize_timers();
-
-Serial.begin(115200);
-pinMode(13, OUTPUT);
-
-pinMode(Esc_Gen_Load, OUTPUT);
-pinMode(Gen_Esc_Rotor, OUTPUT);
-pinMode(Esc_Power, OUTPUT);
-pinMode(Ecu_Power, OUTPUT);
-
+  initialize_servo();
+  initialize_timers();
+  
+  // INITIALIZE BUFFEREDSERIAL
+  //serial.init(0, 115200);
+  serial.init(0, 9600);
+  serial.setPacketHandler(handlePacket);
+  send_buffer.init(64);
+  
+  pinMode(Esc_Gen_Load, OUTPUT);
+  pinMode(Gen_Esc_Rotor, OUTPUT);
+  pinMode(Esc_Power, OUTPUT);
+  pinMode(Ecu_Power, OUTPUT);
 }
 
 void loop() {
 
-digitalWrite(Esc_Gen_Load,HIGH);  
-delay(500);
-digitalWrite(Esc_Gen_Load,LOW);  
-delay(500);
-if (timer_interrupt_flag)
-  {
-    uint32_t tach_speed;
-    if (!stopped_flag&&timeFromLastPulse>0)
-    {
+  digitalWrite(Esc_Gen_Load,HIGH);  
+  delay(500);
+  digitalWrite(Esc_Gen_Load,LOW);  
+  delay(500);
+  
+  serial.update();
+  if (controller_update_flag) {
+    controller_update_flag = 0;
+    if (!stopped_flag && timeFromLastPulse>0) {
       tach_speed = 3750000 / timeFromLastPulse;
-    }
-    else 
-    {
+      servo_angle = servo_angle + (uint8_t)(Kp*LSF*(setpoint_speed - tach_speed));
+      servo_angle = min(max(servo_angle,LOWER_SERVO_TH),UPPER_SERVO_TH);
+      //servo.write(servo_angle);
+    } else {
       tach_speed = 0;
     }
-    Serial.print(timeFromLastPulse);
-    Serial.print("   ");
-    Serial.println(tach_speed);
-    timer_interrupt_flag = 0;
-/*
-    PORTB |= (1<<PB5);    // test signal  120RPM
-    delay(10);         
-    PORTB &= ~(1<<PB5);
-    */
+  }
+  
+  if (serial_write_counter >= serial_write_threshold) {
+    serial_write_counter = 0;
+    
+    // WRITE TO BUFFEREDSERIAL
+    send_buffer.clear();
+    send_buffer.putFloat((float)tach_speed);
+    send_buffer.putFloat((float)servo_angle);
+    serial.sendSerialPacket( &send_buffer );
   }
 }
 
 //ISRs
 
-ISR(TIMER5_CAPT_vect)  // PULSE DETECTED 
-{
-  if(PINL & (1<<PL1))//check to see if rising or falling edge
-  {
+ISR(TIMER5_CAPT_vect) { // PULSE DETECTED 
+  if(PINL & (1<<PL1)) { //check to see if rising or falling edge
     TCNT5 = 0;// restart timer for next revolution
 
-    if(!stopped_flag) //from stop, dont calculate speed from first pulse
-    {
+    if(!stopped_flag) { //from stop, dont calculate speed from first pulse
       timeFromLastPulse = ICR5;//H << 8 + ICR5L;      // save duration of last revolution   
     }
 
@@ -112,26 +134,32 @@ ISR(TIMER5_CAPT_vect)  // PULSE DETECTED
     PORTB ^= 1 << PORTB7; //blink
 
     TCCR5B &= ~(1 << ICES5); //set the ICP to trigger on the falling edge
-  }
-  else
-  {
+  } else {
     lowPulseTime = ICR5;//records time at start of low pulse, doesnt reset timer
     TCCR5B |= 1 << ICES5; //set the ICP to trigger on the rising edge
   }  
-    
-  
-  
 }
 
-ISR(TIMER5_OVF_vect)    // counter overflow/timeout. Basically this will happen if the engine is stopped
-{ 
+ISR(TIMER5_OVF_vect) { // counter overflow/timeout. Basically this will happen if the engine is stopped 
   stopped_flag = 1;
   timeFromLastPulse = 0xFFFF;
   PORTB ^= 1 << PORTB7; //blink
 }     // engine stopped
 
 
-void sendSerial_Timer_ISR() //trigger every certain amount of time
-{
-  timer_interrupt_flag = 1;
+void setFlags_Timer_ISR() { // trigger every certain amount of time
+  controller_update_flag = 1;
+  serial_write_counter += 1;
+}
+
+void handlePacket(ByteBuffer* packet) {
+  send_buffer.clear();
+  int protocol = (int)packet->getFloat();
+  if (protocol == 1) {
+    servo_angle = (uint8_t)packet->getFloat();
+    servo_angle = min(max(servo_angle,LOWER_SERVO_TH),UPPER_SERVO_TH);
+  } else if (protocol == 2) {
+    setpoint_speed = (uint32_t)packet->getFloat();
+    setpoint_speed = min(max(setpoint_speed,LOWER_RPM_TH),UPPER_RPM_TH);
+  }
 }
